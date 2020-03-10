2c656abde4ec166e139272886f7603d25c69357e
DrGooseman <jamesjquinn12@gmail.com>
2020-03-10 14:13:10 +0300

    adds sentences

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 //app.use(cors());
 
 app.use((req, res, next) => {
-
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
--- utils/word-helper.js
+++ utils/word-helper.js
@@ -1,20 +1,20 @@
 const { Word } = require("../models/wordlist");
+const { wordsArray, translationsArray } = require("./word-list");
 
 function getWords(words, num) {
   let newArray = [];
   let count = 0;
   const now = new Date();
 
-
- for (let i = 0; i < words.length; i++){
+  for (let i = 0; i < words.length; i++) {
     if (new Date(words[i].nextDate) < now) {
       newArray.push(words[i]);
       count++;
     }
     if (count > num) return newArray;
- }
   }
- 
+}
+
 function changeWordLevel(word, level) {
   word.level = level;
   if (level == 0) word.nextDate = new Date();
@@ -28,9 +28,8 @@ function changeWordLevel(word, level) {
     word.nextDate = new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 21);
 }
 
-async function getExpandedWords(words)
-{
-	 const newWords = [];
+async function getExpandedWords(words) {
+  const newWords = [];
   const wordList = await Word.find();
 
   words.forEach(word => {
@@ -49,24 +48,37 @@ async function getExpandedWords(words)
   return newWords;
 }
 
-async function getExpandedWordsLight(words)
-{
-	 const newWords = [];
+async function getExpandedWordsLight(words) {
+  const newWords = [];
   const wordList = await Word.find();
 
   words.forEach(word => {
     const foundWord = wordList.find(lWord => lWord.number == word.number);
 
     if (foundWord) {
+      const { sentence, translatedSentence } = findSentence(foundWord.word);
       newWords.push({
         number: word.number,
         level: word.level,
         word: foundWord.word,
-        definition: foundWord.definition
+        definition: foundWord.definition,
+        sentence: sentence,
+        translatedSentence: translatedSentence
       });
     }
   });
   return newWords;
+}
+
+function findSentence(word) {
+  for (let i = 0; i < wordsArray.length; i++) {
+    if (wordsArray[i].search(new RegExp(` ${word}[ !.,]`,"i")) > -1)
+      return {
+        sentence: wordsArray[i],
+        translatedSentence: translationsArray[i]
+      };
+  }
+  return { sentence: undefined, translatedSentence: undefined };
 }
 
 exports.getWords = getWords;
